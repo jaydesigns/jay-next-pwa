d85f343e78e9eeae70eaa91f3eab1ebea908e3b8
Jay Indino <45135991+jaydesigns@users.noreply.github.com>
2023-01-07 22:00:29 -0700

    added offline fallback

--- next.config.js
+++ next.config.js
@@ -2,10 +2,9 @@ const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
   register: true,
-  //scope: '/app',
-  //sw: 'sw.js',
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development"
+  scope: '/app',
+  sw: 'sw.js',
+  skipWaiting: true
   //...
 })
 
